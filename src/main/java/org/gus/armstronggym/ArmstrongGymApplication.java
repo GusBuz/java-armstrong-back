20fd34a95ade1f31989512336ae3715ddaaa7f3c
GusBuzana <gubuzana@gmail.com>
2023-10-23 21:23:59 -0300

    first commit

---
+++ src/main/java/org/gus/armstronggym/ArmstrongGymApplication.java
@@ -0,0 +1,13 @@
+package org.gus.armstronggym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmstrongGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmstrongGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gus/armstronggym/ArmstrongGymApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gus.armstronggym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmstrongGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
